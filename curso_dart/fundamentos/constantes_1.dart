68075bf3c023c211d392ff2846a510bd1bd27afc
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-09-14 20:10:32 -0300

    Dart - Fundamentos: Constantes #01

---
+++ curso_dart/fundamentos/constantes_1.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+main() {
+  //Área da cincunferência = PI * raio * raio
+  const PI = 3.1415;
+
+  stdout.write("Informe o raio: ");
+  //Ctrl + Alt + N -> Run
+  //Ctrl + Alt + M -> Stop
+  final entradaDoUsuario = stdin.readLineSync();
+  final double raio = double.parse(entradaDoUsuario);
+  final area = PI * raio * raio;
+  print("O valor da área é: " + area.toString());
+}
