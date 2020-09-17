69ea8493bc71daec1846a37c0da7f90a6bf78a8c
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-09-17 17:40:11 -0300

    Dart - Estruturas de Controle: For #02

--- curso_dart/controle/for_1.dart
+++ curso_dart/controle/for_1.dart
@@ -13,5 +13,11 @@ main() {
   }
 
   print('[FORA] b = $b');
+
+  var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
+  for (var i = 0; i < notas.length; i++) {
+    print('Nota ${i + 1} = ${notas[i]}');
+  }
+
   print('Fim');
 }
