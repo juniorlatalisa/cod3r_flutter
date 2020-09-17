c749dda030c54b6e7970cc63d9ad11afc183a898
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-09-17 19:20:59 -0300

    Dart - Estruturas de Controle: Desafio For

---
+++ curso_dart/controle/desafio_for.dart
@@ -0,0 +1,22 @@
+/*
+#
+##
+###
+####
+#####
+######
+Use o laço FOR, mas não pode controlar o laço usando valor númerico
+*/
+main() {
+  print('Solução 01');
+  for (var value = '#'; value != '#######'; value += '#') {
+    print(value);
+  }
+  print('Solução 02');
+  for (var value = '';;) {
+    print(value += '#');
+    if (value == '######') {
+      break;
+    }
+  }
+}
