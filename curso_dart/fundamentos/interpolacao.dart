cd8197bbc49105fbb31b1c1be6c26dfd293dda3f
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-09-16 21:00:01 -0300

    Dart - Fundamentos: Interpolação

---
+++ curso_dart/fundamentos/interpolacao.dart
@@ -0,0 +1,15 @@
+main() {
+  String nome = 'João';
+  String status = 'Aprovado';
+  double nota = 9.2;
+
+  String frase1 =
+      nome + " está " + status + " pq tirou nota " + nota.toString() + "!";
+  String frase2 = "$nome está $status pq tirou nota $nota!";
+
+  print(frase1);
+  print(frase2);
+  print(frase1 == frase2);
+
+  print("30 * 30 = ${30 * 30}");
+}
