25b18cf49010d97cac9e0717fd8c139e1463ad7a
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-09-17 17:48:19 -0300

    Dart - Estruturas de Controle: For #03

---
+++ curso_dart/controle/for_3.dart
@@ -0,0 +1,21 @@
+main(List<String> args) {
+  Map<String, double> notas = {
+    'João Pedro': 9.1,
+    'Maria Augusta': 7.2,
+    'Ana Silva': 6.4,
+    'Roberto Andrade': 8.8,
+    'Pedro Firmino': 9.9,
+  };
+
+  for (String nome in notas.keys) {
+    print("O nome do aluno é $nome e a nota é ${notas[nome]}");
+  }
+
+  for (var nota in notas.values) {
+    print("A nota é $nota");
+  }
+
+  for (var registro in notas.entries) {
+    print("O ${registro.key} tem nota ${registro.value}.");
+  }
+}
