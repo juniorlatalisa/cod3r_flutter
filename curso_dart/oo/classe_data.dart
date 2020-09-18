9dd81a3f0b274fb9be43599c4538bb98d37acee5
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-09-18 14:37:52 -0300

    Dart - Orientação a Objetos: Construtores

--- curso_dart/oo/classe_data.dart
+++ curso_dart/oo/classe_data.dart
@@ -3,6 +3,14 @@ class Data {
   int mes;
   int ano;
 
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+
   String obterFormatado() {
     return "$dia/$mes/$ano";
   }
@@ -15,19 +23,23 @@ class Data {
 }
 
 main() {
-  var dataAniversario = new Data();
-  dataAniversario.dia = 3;
-  dataAniversario.mes = 10;
-  dataAniversario.ano = 2020;
+  var dataAniversario = new Data(3, 10, 2020);
+  // dataAniversario.dia = 3;
+  // dataAniversario.mes = 10;
+  // dataAniversario.ano = 2020;
   // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
   print(dataAniversario.obterFormatado());
 
+  // Data dataCompra = Data(1, 1, 1970);
   Data dataCompra = Data();
-  dataCompra.dia = 23;
+  // dataCompra.dia = 23;
   dataCompra.mes = 12;
-  dataCompra.ano = 2021;
+  dataCompra.ano = 1999;
   // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
   print(dataCompra);
 
   print(Data().toString());
+  print(Data(31).toString());
+  print(Data(31, 12).toString());
+  print(Data(31, 12, 2021).toString());
 }
