f30fbe5ed42ed812c2ef45a281081aa2c38ab199
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-09-18 13:24:16 -0300

    Dart - Orientação a Objetos: Métodos

--- curso_dart/oo/classe_data.dart
+++ curso_dart/oo/classe_data.dart
@@ -2,6 +2,16 @@ class Data {
   int dia;
   int mes;
   int ano;
+
+  String obterFormatado() {
+    return "$dia/$mes/$ano";
+  }
+
+  String toString() {
+    return (dia == null || mes == null || ano == null)
+        ? super.toString()
+        : obterFormatado();
+  }
 }
 
 main() {
@@ -9,11 +19,15 @@ main() {
   dataAniversario.dia = 3;
   dataAniversario.mes = 10;
   dataAniversario.ano = 2020;
-  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  print(dataAniversario.obterFormatado());
 
   Data dataCompra = Data();
   dataCompra.dia = 23;
   dataCompra.mes = 12;
   dataCompra.ano = 2021;
-  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+  // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+  print(dataCompra);
+
+  print(Data().toString());
 }
