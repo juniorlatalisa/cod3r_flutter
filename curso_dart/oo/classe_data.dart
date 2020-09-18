03a77d5958ad504c60c1713c230a46d599f3fefb
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-09-18 14:47:18 -0300

    Dart - Orientação a Objetos: Construtores Nomeados

--- curso_dart/oo/classe_data.dart
+++ curso_dart/oo/classe_data.dart
@@ -3,13 +3,16 @@ class Data {
   int mes;
   int ano;
 
-  // Data(int dia, int mes, int ano) {
-  //   this.dia = dia;
-  //   this.mes = mes;
-  //   this.ano = ano;
-  // }
-
   Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+
+  Data.com({this.dia = 1, this.mes = 1, this.ano = 1970});
+
+  Data.ultimoDiaDoAno(this.ano) {
+    // Data.ultimoDiaDoAno(int ano) {
+    this.dia = 31;
+    this.mes = 12;
+    // this.ano = ano;
+  }
 
   String obterFormatado() {
     return "$dia/$mes/$ano";
@@ -24,22 +27,22 @@ class Data {
 
 main() {
   var dataAniversario = new Data(3, 10, 2020);
-  // dataAniversario.dia = 3;
-  // dataAniversario.mes = 10;
-  // dataAniversario.ano = 2020;
-  // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
   print(dataAniversario.obterFormatado());
 
-  // Data dataCompra = Data(1, 1, 1970);
   Data dataCompra = Data();
-  // dataCompra.dia = 23;
   dataCompra.mes = 12;
   dataCompra.ano = 1999;
-  // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
   print(dataCompra);
 
   print(Data().toString());
   print(Data(31).toString());
   print(Data(31, 12).toString());
   print(Data(31, 12, 2021).toString());
+
+  print(Data.com(ano: 2022));
+
+  var dataMickey = Data.com(ano: 2024, mes: 7, dia: 12);
+  print("O Mickey será público em $dataMickey");
+
+  print(Data.ultimoDiaDoAno(2023));
 }
