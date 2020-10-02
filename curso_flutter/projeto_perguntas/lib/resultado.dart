2831af039c285a0fb362d0e22e23fb70dce017aa
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-10-01 22:20:36 -0300

    Noções básicas de Flutter: Desafio Widget Resultado - Resposta

--- curso_flutter/projeto_perguntas/lib/resultado.dart
+++ curso_flutter/projeto_perguntas/lib/resultado.dart
@@ -3,15 +3,13 @@ import 'package:flutter/material.dart';
 class Resultado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
-        child: Center(
-          child: Text(
-            'Parabéns',
-            style: TextStyle(
-              fontSize: 28,
-            ),
-          ),
-        ));
+    return Center(
+      child: Text(
+        'Parabéns',
+        style: TextStyle(
+          fontSize: 28,
+        ),
+      ),
+    );
   }
 }
