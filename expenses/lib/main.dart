e85ad0192b4a5b3ed0c09485907e69014116a84b
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-10-12 18:43:20 -0300

    Construindo App Real - Despesas Pessoais: Aplicando Estilo no Texto

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -56,17 +56,26 @@ class MyHomePage extends StatelessWidget {
                             ),
                             decoration: BoxDecoration(
                               border: Border.all(
-                                color: Colors.black,
+                                color: Colors.purple,
                                 width: 2,
                               ),
                             ),
                             padding: EdgeInsets.all(10),
-                            child: Text(tr.value.toString()),
+                            child: Text(
+                              tr.value.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.purple,
+                                fontSize: 20,
+                              ),
+                            ),
                           ),
                           Column(
                             children: <Widget>[
                               Text(tr.title),
-                              Text(tr.date.toString()),
+                              Text(
+                                tr.date.toString(),
+                              ),
                             ],
                           ),
                         ],
