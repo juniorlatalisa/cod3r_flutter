7178238244fe3993806d704a31e7c6ab10c36e01
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-11-15 19:14:49 -0300

    Construindo App Real - Despesas Pessoais: Usando ListTile

--- curso_flutter/expenses/lib/components/transaction_list.dart
+++ curso_flutter/expenses/lib/components/transaction_list.dart
@@ -7,48 +7,74 @@ class TransactionList extends StatelessWidget {
 
   TransactionList(this.transactions);
 
+  final _format = DateFormat('d MMM y');
+
   _createCard(BuildContext context, Transaction tr) => Card(
-        child: Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: 15,
-                vertical: 10,
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Theme.of(context).primaryColor,
-                  width: 2,
-                ),
-              ),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                'R\$ ${tr.value.toStringAsFixed(2)}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 20,
-                ),
+        elevation: 5,
+        margin: EdgeInsets.symmetric(
+          vertical: 8,
+          horizontal: 5,
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: FittedBox(
+                child: Text('R\$${tr.value.toStringAsFixed(2)}'),
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  tr.title,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                Text(
-                  DateFormat('d MMM y').format(tr.date),
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          ],
+          ),
+          title: Text(
+            tr.title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          subtitle: Text(_format.format(tr.date)),
         ),
       );
+
+  // _createCard(BuildContext context, Transaction tr) => Card(
+  //       child: Row(
+  //         children: <Widget>[
+  //           Container(
+  //             margin: EdgeInsets.symmetric(
+  //               horizontal: 15,
+  //               vertical: 10,
+  //             ),
+  //             decoration: BoxDecoration(
+  //               border: Border.all(
+  //                 color: Theme.of(context).primaryColor,
+  //                 width: 2,
+  //               ),
+  //             ),
+  //             padding: EdgeInsets.all(10),
+  //             child: Text(
+  //               'R\$ ${tr.value.toStringAsFixed(2)}',
+  //               style: TextStyle(
+  //                 fontWeight: FontWeight.bold,
+  //                 color: Theme.of(context).primaryColor,
+  //                 fontSize: 20,
+  //               ),
+  //             ),
+  //           ),
+  //           Column(
+  //             crossAxisAlignment: CrossAxisAlignment.start,
+  //             children: <Widget>[
+  //               Text(
+  //                 tr.title,
+  //                 style: Theme.of(context).textTheme.headline6,
+  //               ),
+  //               Text(
+  //                 DateFormat('d MMM y').format(tr.date),
+  //                 style: TextStyle(
+  //                   color: Colors.grey,
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //         ],
+  //       ),
+  //     );
 
   @override
   Widget build(BuildContext context) {
