a26bc91aa9ba19eb7ff00b04773c51b1f17ab8bc
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-11-15 19:39:51 -0300

    Construindo App Real - Despesas Pessoais: Preparando Formulário

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -12,12 +12,15 @@ class ExpensesApp extends StatelessWidget {
           accentColor: Colors.amber,
           fontFamily: 'Quicksand',
           textTheme: ThemeData.light().textTheme.copyWith(
-                headline6: TextStyle(
-                  fontFamily: 'OpenSans',
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
+              button: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
           appBarTheme: AppBarTheme(
             textTheme: ThemeData.light().textTheme.copyWith(
                   headline6: TextStyle(
