8f67fc98fcce458d5b6e076290e4269832457b98
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-12-02 20:54:36 -0300

    Interfaces Responsivas & Adaptativas: Ajustando Formulário

--- curso_flutter/expenses/lib/components/transaction_form.dart
+++ curso_flutter/expenses/lib/components/transaction_form.dart
@@ -41,62 +41,69 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              // onChanged: (newValue) => title = newValue,
-              controller: _title,
-              decoration: InputDecoration(
-                labelText: 'Título',
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: 10 + MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                // onChanged: (newValue) => title = newValue,
+                controller: _title,
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                ),
               ),
-            ),
-            TextField(
-              // onChanged: (newValue) => value = newValue,
-              controller: _value,
-              //onSubmitted: (value) => _submitForm(),
-              onSubmitted: (_) => _submitForm(), // _ para ignorar o parametro
-              keyboardType: TextInputType.numberWithOptions(
-                decimal: true, //Permitir decimal no ios
+              TextField(
+                // onChanged: (newValue) => value = newValue,
+                controller: _value,
+                //onSubmitted: (value) => _submitForm(),
+                onSubmitted: (_) => _submitForm(), // _ para ignorar o parametro
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true, //Permitir decimal no ios
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Valor (R\$)',
+                ),
               ),
-              decoration: InputDecoration(
-                labelText: 'Valor (R\$)',
-              ),
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      'Data selecionada:  ${DateFormat('dd/MM/y').format(_dataSelecionada)}',
-                    ),
-                  ),
-                  FlatButton(
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text(
-                      'Selecionar data',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+              Container(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        'Data selecionada:  ${DateFormat('dd/MM/y').format(_dataSelecionada)}',
                       ),
                     ),
-                    onPressed: _showDatePicker,
-                  )
-                ],
+                    FlatButton(
+                      textColor: Theme.of(context).primaryColor,
+                      child: Text(
+                        'Selecionar data',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: _showDatePicker,
+                    )
+                  ],
+                ),
               ),
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-              RaisedButton(
-                child: Text('Nova Transação'),
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).textTheme.button.color,
-                onPressed: () => _submitForm(),
-              )
-            ]),
-          ],
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+                RaisedButton(
+                  child: Text('Nova Transação'),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).textTheme.button.color,
+                  onPressed: () => _submitForm(),
+                )
+              ]),
+            ],
+          ),
         ),
       ),
     );
