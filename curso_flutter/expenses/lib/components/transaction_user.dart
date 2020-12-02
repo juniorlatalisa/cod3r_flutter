71ebaa29404b43faa18732cf1763416e7ba1264f
juniorlatalisa <juniorlatalisa@MacBook-Pro-de-Junior.local>
2020-12-01 21:21:23 -0300

    Interfaces Responsivas & Adaptativas: Modo Paisagem #03

--- curso_flutter/expenses/lib/components/transaction_user.dart
+++ curso_flutter/expenses/lib/components/transaction_user.dart
@@ -86,13 +86,23 @@ class _TransactionsUserState extends State<TransactionsUser> {
       ],
     );
     final _mqd = MediaQuery.of(context);
+    final _isLandscape = _mqd.orientation == Orientation.landscape;
     final _height =
         _mqd.size.height - _mqd.padding.top - _appBar.preferredSize.height;
-    final _chartHeight = _height * 0.3;
+    final _chartHeight = _height * (_isLandscape ? 0.6 : 0.3);
     final _chartHeightMin = 150.0;
     final _chart = Container(
       height: _chartHeight < _chartHeightMin ? _chartHeightMin : _chartHeight,
       child: Chart(_recentTransactions),
+    );
+    final _switch = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Exibir gráfico'),
+        Switch(
+            value: showGrafico,
+            onChanged: (newValue) => setState(() => showGrafico = newValue))
+      ],
     );
     final _list = Container(
       height: _height -
@@ -106,18 +116,9 @@ class _TransactionsUserState extends State<TransactionsUser> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Exibir gráfico'),
-                Switch(
-                    value: showGrafico,
-                    onChanged: (newValue) =>
-                        setState(() => showGrafico = newValue))
-              ],
-            ),
-            if (showGrafico) _chart,
-            _list,
+            if (_isLandscape) _switch,
+            if (showGrafico || !_isLandscape) _chart,
+            if (!showGrafico || !_isLandscape) _list,
           ],
         ),
       ),
