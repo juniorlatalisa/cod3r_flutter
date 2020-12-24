5b2e505b18fc1b63ec2e12a121a2dfdfdd40e52a
juniorlatalisa <paraojuniorler@gmail.com>
2020-12-23 22:28:29 -0300

    Navegação & Múltiplas Telas: Componente Item Categoria

--- curso_dart/meals/lib/components/category_item.dart
+++ curso_dart/meals/lib/components/category_item.dart
@@ -10,6 +10,18 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Text(category.title),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+        ),
+      ),
     );
   }
 }
