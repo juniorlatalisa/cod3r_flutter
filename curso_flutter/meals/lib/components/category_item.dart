e74f6212cc5e944e6a09a4361ce44667e641a7ee
juniorlatalisa <paraojuniorler@gmail.com>
2020-12-31 11:59:14 -0300

    Navegação & Múltiplas Telas: Navegando para uma Nova Tela

--- curso_flutter/meals/lib/components/category_item.dart
+++ curso_flutter/meals/lib/components/category_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
@@ -8,21 +9,31 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
+    final borderRadius = BorderRadius.circular(15);
+    final theme = Theme.of(context);
+    final navigator = Navigator.of(context);
+    return InkWell(
+      onTap: () => navigator.push(MaterialPageRoute(
+        builder: (_) => CategoriesMealScreeen(),
+      )),
+      splashColor: theme.primaryColor,
+      borderRadius: borderRadius,
+      child: Container(
+        child: Text(
+          category.title,
+          style: theme.textTheme.headline6,
+        ),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: borderRadius,
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+          ),
         ),
       ),
     );
