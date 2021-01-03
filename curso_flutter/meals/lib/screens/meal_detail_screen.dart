b53950dc94a50bc39718600c30ec2b2f30115133
juniorlatalisa <paraojuniorler@gmail.com>
2021-01-03 11:53:47 -0300

    Navegação & Múltiplas Telas: Tela Detalhe Refeição #01

--- curso_flutter/meals/lib/screens/meal_detail_screen.dart
+++ curso_flutter/meals/lib/screens/meal_detail_screen.dart
@@ -7,13 +7,52 @@ class MealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Meal meal = ModalRoute.of(context).settings.arguments;
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(
-        child: Text('Detalhes da refeição'),
-      ),
+      body: Column(children: <Widget>[
+        Container(
+          height: 300,
+          width: double.infinity,
+          child: Image.network(
+            meal.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          child: Text(
+            'Ingredientes',
+            style: theme.textTheme.headline6,
+          ),
+        ),
+        Container(
+          width: 300,
+          height: 200,
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.grey),
+          ),
+          child: ListView.builder(
+            itemCount: meal.ingredients.length,
+            itemBuilder: (context, index) => Card(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 10,
+                ),
+                child: Text(meal.ingredients[index]),
+              ),
+              color: theme.accentColor,
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
