216e4fe11efa9a68a61dc54182e16b3ac8590f30
juniorlatalisa <paraojuniorler@gmail.com>
2021-01-04 21:32:46 -0300

    Navegação & Múltiplas Telas: Adicionar TabBar na Parte Inferior

--- curso_flutter/meals/lib/screens/tabs_screen.dart
+++ curso_flutter/meals/lib/screens/tabs_screen.dart
@@ -2,36 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen();
 
   @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Widget> _screens = [
+    CategoriesScreen(),
+    FavoriteScreen(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Vamos cozinhar?'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: const Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: const Icon(Icons.star),
-                text: 'Favoritos',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            const CategoriesScreen(),
-            const FavoriteScreen(),
-          ],
-        ),
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text((_selectedScreenIndex == 0)
+            ? 'Lista de Categorias'
+            : 'Meus Favoritos'),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => setState(() => _selectedScreenIndex = index),
+        backgroundColor: theme.primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: theme.accentColor,
+        currentIndex: _selectedScreenIndex,
+        // type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.category),
+            label: 'Categorias',
+            // title: Text('Categorias'),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.star),
+            label: 'Favoritos',
+            // title: Text('Favoritos'),
+          ),
+        ],
+      ),
+      body: _screens[_selectedScreenIndex],
     );
+    // return DefaultTabController(
+    //   length: 2,
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: const Text('Vamos cozinhar?'),
+    //       bottom: TabBar(
+    //         tabs: <Widget>[
+    //           Tab(
+    //             icon: const Icon(Icons.category),
+    //             text: 'Categorias',
+    //           ),
+    //           Tab(
+    //             icon: const Icon(Icons.star),
+    //             text: 'Favoritos',
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(
+    //       children: <Widget>[
+    //         const CategoriesScreen(),
+    //         const FavoriteScreen(),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
