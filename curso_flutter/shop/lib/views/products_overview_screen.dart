4790352a4dc5c7bb7d413374d6b663e486037dc1
juniorlatalisa <paraojuniorler@gmail.com>
2021-01-19 22:17:35 -0300

    Gerenciamento de Estado: Componente Grid de Produtos

--- curso_flutter/shop/lib/views/products_overview_screen.dart
+++ curso_flutter/shop/lib/views/products_overview_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Lojja'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (_, index) => Text(loadedProducts[index].title),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
   }
 }
