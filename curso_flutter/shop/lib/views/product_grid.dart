c2ecd07239dc44d8adb3156388ce10016d6058b3
juniorlatalisa <paraojuniorler@gmail.com>
2021-01-25 20:47:22 -0300

    Gerenciamento de Estado: Extraindo Grid de Produtos

---
+++ curso_flutter/shop/lib/views/product_grid.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/providers/products.dart';
+import 'package:shop/widgets/product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Products products = Provider.of(context);
+    final List<Product> loadedProducts = products.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadedProducts.length,
+      itemBuilder: (_, index) => ProductItem(loadedProducts[index]),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
--- curso_flutter/shop/lib/views/products_overview_screen.dart
+++ curso_flutter/shop/lib/views/products_overview_screen.dart
@@ -1,29 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/models/product.dart';
-import 'package:shop/providers/products.dart';
-import 'package:shop/widgets/product_item.dart';
+import 'package:shop/views/product_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Products products = Provider.of(context);
-    final List<Product> loadedProducts = products.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (_, index) => ProductItem(loadedProducts[index]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
