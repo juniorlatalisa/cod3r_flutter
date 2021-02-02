b8e2c165a56713ccaa64724ff4d0122ae8eb6e95
juniorlatalisa <paraojuniorler@gmail.com>
2021-02-01 23:15:25 -0300

    Gerenciamento de Estado: MultiProvider

--- curso_flutter/shop/lib/main.dart
+++ curso_flutter/shop/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/providers/cart.dart';
 import 'package:shop/providers/products.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:shop/views/product_datail_screen.dart';
@@ -10,8 +11,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Products()),
+        ChangeNotifierProvider(create: (_) => Cart()),
+      ],
       child: MaterialApp(
         title: 'Minha Loja',
         theme: ThemeData(
