709d2526957396c475b257c3f35c2d8c12be1c7a
juniorlatalisa <paraojuniorler@gmail.com>
2021-02-24 21:16:26 -0300

    Gerenciamento de Estado: Componente Pedidos #01

--- curso_flutter/shop/lib/utils/app_routes.dart
+++ curso_flutter/shop/lib/utils/app_routes.dart
@@ -1,4 +1,6 @@
 class AppRoutes {
+  static const HOME = '/';
   static const PRODUCT_DETAIL = '/product-detail';
   static const CART_DETAIL = '/cart';
+  static const ORDERS = '/orders';
 }
---
+++ curso_flutter/shop/lib/views/orders_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class OrdersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus Pedidos'),
+      ),
+    );
+  }
+}
