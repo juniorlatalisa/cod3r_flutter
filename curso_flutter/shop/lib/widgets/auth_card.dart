65913df395e7faf1225af3d6e5b81b11688a9a1f
juniorlatalisa <paraojuniorler@gmail.com>
2021-07-28 22:15:14 -0300

    Adicionando Animação: Usando Widget AnimatedBuilder

--- curso_flutter/shop/lib/widgets/auth_card.dart
+++ curso_flutter/shop/lib/widgets/auth_card.dart
@@ -38,7 +38,7 @@ class _AuthCardState extends State<AuthCard>
       curve: Curves.linear,
     ));
 
-    _heightAnimation.addListener(() => setState(() {}));
+    // _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -114,92 +114,93 @@ class _AuthCardState extends State<AuthCard>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
-        width: _isLoading ? 100 : deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        // height: _isLoading
-        //     ? 100
-        //     : ((AuthMode.Signup == _authMode ? 340 : 270) *
-        //         (_isValid ? 1.0 : 1.2)),
-        height: _heightAnimation.value.height,
-        child: _isLoading
-            ? CircularProgressIndicator(
-                strokeWidth: 0.9,
-              )
-            : Form(
-                key: _form,
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'e-mail'),
-                      keyboardType: TextInputType.emailAddress,
-                      onSaved: (value) => _authData['email'] = value,
-                      validator: (value) => (value == null ||
-                              value.isEmpty ||
-                              !value.contains('@'))
-                          ? 'Informe um e-mail válido!'
-                          : null,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'Senha'),
-                      controller: _passwordController,
-                      obscureText: true,
-                      onSaved: (value) => _authData['password'] = value,
-                      validator: (value) => (value == null || value.length < 5)
-                          ? 'Informe uma senha válida!'
-                          : null,
-                    ),
-                    if (_authMode == AuthMode.Signup)
-                      TextFormField(
-                        decoration:
-                            InputDecoration(labelText: 'Confirmar senha'),
-                        obscureText: true,
-                        validator: (value) => (value == null ||
-                                value.isEmpty ||
-                                value != _passwordController.text)
-                            ? 'Confirme a senha digitada'
-                            : null,
-                      ),
-                    // SizedBox(height: 20),
-                    Spacer(),
-                    TextButton(
-                      child: Text(
-                        _authMode == AuthMode.Login ? 'Entrar' : 'Registrar',
-                        style: TextStyle(
-                            color: theme.primaryTextTheme.button.color),
-                      ),
-                      onPressed: _submit,
-                      // style: ButtonStyle(
-                      //   backgroundColor:
-                      //       MaterialStateProperty.all<Color>(theme.primaryColor),
-                      //   textStyle: MaterialStateProperty.all<TextStyle>(
-                      //       theme.primaryTextTheme.button),
-                      //   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      //     RoundedRectangleBorder(
-                      //       borderRadius: BorderRadius.circular(30),
-                      //     ),
-                      //   ),
-                      // ),
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 30.0,
-                          vertical: 8.0,
-                        ),
-                        backgroundColor: theme.primaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                    ),
-                    TextButton(
-                      child: Text(
-                        _authMode == AuthMode.Login ? 'Registrar' : 'Login',
-                      ),
-                      onPressed: _switchAuthMode,
-                    ),
-                  ],
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'e-mail'),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (value) => _authData['email'] = value,
+                validator: (value) =>
+                    (value == null || value.isEmpty || !value.contains('@'))
+                        ? 'Informe um e-mail válido!'
+                        : null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Senha'),
+                controller: _passwordController,
+                obscureText: true,
+                onSaved: (value) => _authData['password'] = value,
+                validator: (value) => (value == null || value.length < 5)
+                    ? 'Informe uma senha válida!'
+                    : null,
+              ),
+              if (_authMode == AuthMode.Signup)
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Confirmar senha'),
+                  obscureText: true,
+                  validator: (value) => (value == null ||
+                          value.isEmpty ||
+                          value != _passwordController.text)
+                      ? 'Confirme a senha digitada'
+                      : null,
+                ),
+              // SizedBox(height: 20),
+              Spacer(),
+              TextButton(
+                child: Text(
+                  _authMode == AuthMode.Login ? 'Entrar' : 'Registrar',
+                  style: TextStyle(color: theme.primaryTextTheme.button.color),
+                ),
+                onPressed: _submit,
+                // style: ButtonStyle(
+                //   backgroundColor:
+                //       MaterialStateProperty.all<Color>(theme.primaryColor),
+                //   textStyle: MaterialStateProperty.all<TextStyle>(
+                //       theme.primaryTextTheme.button),
+                //   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                //     RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.circular(30),
+                //     ),
+                //   ),
+                // ),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                    vertical: 8.0,
+                  ),
+                  backgroundColor: theme.primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                 ),
               ),
+              TextButton(
+                child: Text(
+                  _authMode == AuthMode.Login ? 'Registrar' : 'Login',
+                ),
+                onPressed: _switchAuthMode,
+              ),
+            ],
+          ),
+        ),
+        builder: (ctx, child) => Container(
+          width: _isLoading ? 100 : deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          // height: _isLoading
+          //     ? 100
+          //     : ((AuthMode.Signup == _authMode ? 340 : 270) *
+          //         (_isValid ? 1.0 : 1.2)),
+          height: _heightAnimation.value.height * (_isValid ? 1.0 : 1.1),
+          child: _isLoading
+              ? const CircularProgressIndicator(
+                  strokeWidth: 0.9,
+                )
+              : child,
+        ),
       ),
     );
   }
