d47eea75e32631cb91ced811c1d6329d23c6911b
juniorlatalisa <paraojuniorler@gmail.com>
2021-08-24 22:20:29 -0300

    Adicionando Animação: Animando a Tela de Pedidos

--- curso_flutter/shop/lib/widgets/order_item.dart
+++ curso_flutter/shop/lib/widgets/order_item.dart
@@ -17,26 +17,33 @@ class _OrderWidgetState extends State<OrderWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text(widget.order.amount.toString()),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
-            trailing: IconButton(
-              icon: const Icon(Icons.expand_more),
-              onPressed: () => setState(() => _expanded = !_expanded),
+    final itemsHeight = widget.order.products.length * 25.0 + 10;
+    const duration = const Duration(milliseconds: 300);
+    return AnimatedContainer(
+      duration: duration,
+      height: _expanded ? itemsHeight + 100 : 100,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(widget.order.amount.toString()),
+              subtitle: Text(
+                  DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
+              trailing: IconButton(
+                icon: const Icon(Icons.expand_more),
+                onPressed: () => setState(() => _expanded = !_expanded),
+              ),
             ),
-          ),
-          if (_expanded)
-            Container(
+            // if (_expanded)
+            AnimatedContainer(
+              duration: duration,
+              height: _expanded ? itemsHeight : 0,
               padding: const EdgeInsets.symmetric(
                 horizontal: 15,
                 vertical: 4,
               ),
-              height: widget.order.products.length * 25.0 + 10,
+              // height: itemsHeight,
               child: ListView(
                 children: widget.order.products
                     .map(
@@ -63,7 +70,8 @@ class _OrderWidgetState extends State<OrderWidget> {
                     .toList(),
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
