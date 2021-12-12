21300d630d80f64caeb0fc6b1d1b53118c8827ef
juniorlatalisa <paraojuniorler@gmail.com>
2021-12-12 11:09:38 -0300

    Usando Recursos Nativos: Componente LocationInput

--- curso_flutter/great_places/lib/screens/place_form_screen.dart
+++ curso_flutter/great_places/lib/screens/place_form_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/widgets/image_input.dart';
+import 'package:great_places/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 
 class PlaceFormScreen extends StatefulWidget {
@@ -52,6 +53,8 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                   ),
                   const SizedBox(width: 10),
                   ImageInput(_onSelectedImage),
+                  const SizedBox(width: 10),
+                  LocationInput(),
                 ],
               ),
             ),
---
+++ curso_flutter/great_places/lib/widgets/location_input.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _previewImageUrl == null
+              ? const Text('Localização não informada!')
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              icon: Icon(Icons.location_on),
+              label: const Text('Localização atual'),
+              onPressed: () => print(this),
+            ),
+            TextButton.icon(
+              icon: Icon(Icons.map),
+              label: const Text('Selecione no Mapa'),
+              onPressed: () => print(this),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
