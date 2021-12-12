945908a281e62519b97d0e8892139be4ecee70c2
juniorlatalisa <paraojuniorler@gmail.com>
2021-12-12 11:34:03 -0300

    Usando Recursos Nativos: Mostrando Imagem Estática do Mapa

--- curso_flutter/great_places/lib/widgets/location_input.dart
+++ curso_flutter/great_places/lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/utils/location_util.dart';
 import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
@@ -11,8 +12,14 @@ class _LocationInputState extends State<LocationInput> {
 
   Future<void> _getCurrentUserLocation() async {
     final locData = await Location().getLocation();
-    print(locData.latitude);
-    print(locData.longitude);
+    final staticMapImageUrl = LocationUtil.generateLocationPreviewImage(
+      latitude: locData.latitude,
+      longitude: locData.longitude,
+    );
+
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
   }
 
   @override
