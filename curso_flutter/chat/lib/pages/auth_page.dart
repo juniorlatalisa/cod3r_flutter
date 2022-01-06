e9f9497a0b356bb83d1a3c101757412f4bfd3124
Junior Latalisa <paraojuniorler@gmail.com>
2022-01-06 20:51:34 -0300

    Aplicação de Chat - Firebase, Upload Imagem e Push Notifications: Lógica Auth #01

--- curso_flutter/chat/lib/pages/auth_page.dart
+++ curso_flutter/chat/lib/pages/auth_page.dart
@@ -12,13 +12,20 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
 
-    print('AuthPage...');
-    print(formData.email);
-
-    setState(() => _isLoading = false);
+      if (formData.isLogin) {
+        // Login
+      } else {
+        // Signup
+      }
+    } catch (error) {
+      // Tratar erro!
+    } finally {
+      setState(() => _isLoading = false);
+    }
   }
 
   @override
