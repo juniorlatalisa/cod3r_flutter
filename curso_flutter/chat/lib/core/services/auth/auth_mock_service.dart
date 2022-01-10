b2db701f1f5b3bc4dc80cf0517d733847ec5172f
Junior Latalisa <paraojuniorler@gmail.com>
2022-01-10 20:25:18 -0300

    Aplicação de Chat - Firebase, Upload Imagem e Push Notifications: Lógica Auth #04

--- curso_flutter/chat/lib/core/services/auth/auth_mock_service.dart
+++ curso_flutter/chat/lib/core/services/auth/auth_mock_service.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'dart:async';
+import 'dart:math';
 
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
@@ -6,19 +8,47 @@ import 'package:chat/core/services/auth/auth_service.dart';
 class AuthMockService implements AuthService {
   static Map<String, ChatUser> _users = {};
   static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
 
   ChatUser? get currentUser {
     return _currentUser;
   }
 
   Stream<ChatUser?> get userChanges {
-    return Stream.empty();
+    return _userStream;
   }
 
   Future<void> signup(
-      String nome, String email, String password, File image) async {}
+    String name,
+    String email,
+    String password,
+    File image,
+  ) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageURL: image.path,
+    );
 
-  Future<void> login(String email, String password) async {}
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
+  }
 
-  Future<void> logout() async {}
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
+  }
+
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
