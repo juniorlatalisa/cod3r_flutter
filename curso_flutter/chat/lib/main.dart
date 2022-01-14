d1d33b9d07dcea93a5e0d7aa6618df40df68c239
Junior Latalisa <paraojuniorler@gmail.com>
2022-01-14 20:00:38 -0300

    Aplicação de Chat - Firebase, Upload Imagem e Push Notifications: Chat Notification Service #01

--- curso_flutter/chat/lib/main.dart
+++ curso_flutter/chat/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chat/core/services/notification/push_notification_service.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +11,20 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PushNotificationService(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const AuthOrAppPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
