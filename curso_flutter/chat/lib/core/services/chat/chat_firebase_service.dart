bcedfec126a8c9aa673a672885cf06d851f8e3aa
juniorlatalisa <paraojuniorler@gmail.com>
2022-02-10 19:26:45 -0300

    Aplicação de Chat - Firebase, Upload Imagem e Push Notifications: Salvando Mensagens do Firestore #01

---
+++ curso_flutter/chat/lib/core/services/chat/chat_firebase_service.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatFirebaseService implements ChatService {
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    return Stream<List<ChatMessage>>.empty();
+  }
+
+  @override
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+
+    // ChatMessage => Map<String, dynamic>
+    store.collection('chat').add({
+      'text': text,
+      'createdAt': DateTime.now().toIso8601String(),
+      'userId': user.id,
+      'userName': user.name,
+      'userImageURL': user.imageURL,
+    });
+
+    return null;
+  }
+}
