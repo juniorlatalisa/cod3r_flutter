e41f6c7219f26e66ed29d86ed984244513124ce6
juniorlatalisa <paraojuniorler@gmail.com>
2022-03-23 21:57:17 -0300

    Projeto Campo Minado: Componente Campo

---
+++ curso_flutter/campo_minado/lib/components/campo_widget.dart
@@ -0,0 +1,39 @@
+import 'package:campo_minado/models/campo.dart';
+import 'package:flutter/material.dart';
+
+class CampoWidget extends StatelessWidget {
+  final Campo campo;
+  final void Function(Campo) onAbrir;
+  final void Function(Campo) onAlternarMarcacao;
+
+  const CampoWidget({
+    required this.campo,
+    required this.onAbrir,
+    required this.onAlternarMarcacao,
+    Key? key,
+  }) : super(key: key);
+
+  Widget _getImage() {
+    int qtdeMinas = campo.qtdeMinasNaVizinhaca;
+    if (campo.aberto && campo.minado && campo.explodido) {
+      return Image.asset('assets/images/bomba_0.jpeg');
+    } else if (campo.aberto && campo.minado) {
+      return Image.asset('assets/images/bomba_1.jpeg');
+    } else if (campo.aberto) {
+      return Image.asset('assets/images/aberto_$qtdeMinas.jpeg');
+    } else if (campo.marcado) {
+      return Image.asset('assets/images/bandeira.jpeg');
+    } else {
+      return Image.asset('assets/images/fechado.jpeg');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onAbrir(campo),
+      onLongPress: () => onAlternarMarcacao(campo),
+      child: _getImage(),
+    );
+  }
+}
--- curso_flutter/campo_minado/lib/screens/campo_minado_app.dart
+++ curso_flutter/campo_minado/lib/screens/campo_minado_app.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
+import '../components/campo_widget.dart';
+import '../models/campo.dart';
+import '../models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({Key? key}) : super(key: key);
 
-  _reiniciar() {
+  void _reiniciar() {
     print('Reiniciar...');
+  }
+
+  void _abrir(Campo campo) {
+    print('Abrir...');
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    print('Alternar marcação...');
   }
 
   @override
   Widget build(BuildContext context) {
+    Campo vizinho1 = Campo(linha: 1, coluna: 0);
+    vizinho1.minar();
+    Campo vizinho2 = Campo(linha: 1, coluna: 0);
+    vizinho2.minar();
+
+    Campo campo = Campo(linha: 0, coluna: 0);
+    campo.adicionarVizinho(vizinho1);
+    campo.adicionarVizinho(vizinho2);
+
+    try {
+      //campo.minar();
+      campo.alternarMarcacao();
+    } on ExplosaoException {}
+
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
@@ -17,7 +42,11 @@ class CampoMinadoApp extends StatelessWidget {
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: const Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );
