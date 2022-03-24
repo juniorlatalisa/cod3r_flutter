fdd4bc488819db96df882bd0adc4374469929d5e
juniorlatalisa <paraojuniorler@gmail.com>
2022-03-23 21:43:07 -0300

    Projeto Campo Minado: Componente Resultado

--- curso_flutter/campo_minado/lib/screens/campo_minado_app.dart
+++ curso_flutter/campo_minado/lib/screens/campo_minado_app.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import '../components/resultado_widget.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({Key? key}) : super(key: key);
+
+  _reiniciar() {
+    print('Reiniciar...');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Campo Minado'),
+        appBar: ResultadoWidget(
+          venceu: false,
+          onReiniciar: _reiniciar,
         ),
         body: Container(
           child: const Text('Tabuleiro'),
