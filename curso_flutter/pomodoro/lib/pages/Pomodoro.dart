52464fb39c76304fc59016ecb03c1c4ac1537bee
juniorlatalisa <paraojuniorler@gmail.com>
2022-03-31 22:34:51 -0300

    MobX: Desenvolvendo uma Aplicação Completa: Componente Cronômetro

--- curso_flutter/pomodoro/lib/pages/Pomodoro.dart
+++ curso_flutter/pomodoro/lib/pages/Pomodoro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/components/Cronometro.dart';
 import 'package:pomodoro/components/EntradaTempo.dart';
 
 class Pomodoro extends StatelessWidget {
@@ -8,20 +9,26 @@ class Pomodoro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Pomodoro'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              EntradaTempo(
-                titulo: 'Trabalho',
-                valor: 25,
-              ),
-              EntradaTempo(
-                titulo: 'Descanso',
-                valor: 5,
-              ),
-            ],
+          Expanded(
+            child: Cronometro(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                EntradaTempo(
+                  titulo: 'Trabalho',
+                  valor: 25,
+                ),
+                EntradaTempo(
+                  titulo: 'Descanso',
+                  valor: 5,
+                ),
+              ],
+            ),
           ),
         ],
       ),
