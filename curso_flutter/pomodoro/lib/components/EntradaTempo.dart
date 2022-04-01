19efff00ce62b4c024816f4242045731de579406
juniorlatalisa <paraojuniorler@gmail.com>
2022-03-31 22:29:14 -0300

    MobX: Desenvolvendo uma Aplicação Completa: Componente EntradaTempo #01

--- curso_flutter/pomodoro/lib/components/EntradaTempo.dart
+++ curso_flutter/pomodoro/lib/components/EntradaTempo.dart
@@ -13,9 +13,50 @@ class EntradaTempo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(this.titulo),
-        Text('${this.valor}'),
+        Text(
+          this.titulo,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                primary: Colors.red,
+              ),
+            ),
+            Text(
+              '${this.valor} min',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                primary: Colors.red,
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
--- curso_flutter/pomodoro/lib/pages/Pomodoro.dart
+++ curso_flutter/pomodoro/lib/pages/Pomodoro.dart
@@ -10,13 +10,18 @@ class Pomodoro extends StatelessWidget {
       body: Column(
         children: [
           Text('Pomodoro'),
-          EntradaTempo(
-            titulo: 'Trabalho',
-            valor: 25,
-          ),
-          EntradaTempo(
-            titulo: 'Descanso',
-            valor: 5,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              EntradaTempo(
+                titulo: 'Trabalho',
+                valor: 25,
+              ),
+              EntradaTempo(
+                titulo: 'Descanso',
+                valor: 5,
+              ),
+            ],
           ),
         ],
       ),
